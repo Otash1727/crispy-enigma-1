f7da628df4a75b42c506798a91b8248c134ebfd5
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-08-09 01:05:50 +0300

    fix

--- src/components/DashboardView.vue
+++ src/components/DashboardView.vue
@@ -11,7 +11,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="font-PlusJakartaSans flex flex-col justify-between gap-8 text-lg max-w-7xl overflow-y-auto overflow-x-hidden bg-white">
+    <div class="font-PlusJakartaSans flex flex-col justify-between gap-8 text-lg max-w-6xl overflow-y-auto overflow-x-hidden bg-white">
         <div class="flex gap-32">
             <div class="flex flex-col gap-4">
                 <h1 class="text-3xl font-bold">Hi, Skylar Dias</h1>
@@ -20,15 +20,15 @@ defineProps({
             <div class="flex gap-3 p-6">
                 <div class="p-4">
                     <button>
-                        <img src="src/assets/images/SecondLinePictures/Notification.png" alt="">
+                        <img src="src/assets/images/Dashboardpictures/Notification.png" alt="">
                     </button>    
                 </div>
                 <div>
-                    <img src="src/assets/images/SecondLinePictures/Profil.png" alt="" class="w-16">
+                    <img src="src/assets/images/Dashboardpictures/Profil.png" alt="" class="w-16">
                 </div>
             </div>
         </div>
-        <div class="flex">
+        <div class="flex gap-32">
             <div class="bg-[#141522] text-white rounded-lg flex flex-col items-center w-64 h-52 gap-3 justify-center">
                 <div class="flex flex-col gap-3 mr-16"> 
                     <p>Running Task</p>
@@ -42,24 +42,11 @@ defineProps({
                     </div>
                 </div>
             </div>
-            <!-- <div class="flex flex-col gap-6">
-                <p>Activity</p>
-                <div class=" flex outline-offset-8 outline outline-white">
-                    
-                    <div>
-                        <ul>
-                            <li>3</li>
-                            <li>2</li>
-                            <li>1</li>
-                            
-                        </ul>
-                    </div>
-                        <img src="src/assets/images/SecondLinePictures/diagramma.svg" alt="">
-                    </div>
-                    <div class="bg-[#141522] rounded-lg text-white w-16 h-12 ">
-                        <p>2Tasks</p>
-                    </div>
-            </div>           -->
+             <div class="flex flex-col gap-6"> 
+                <p class="text-xl font-bold">Activity</p>
+                <img src="src/assets/images/Dashboardpictures/Diagram.svg" alt="">
+               
+            </div>           
         </div>
         <DashboardContext></DashboardContext>
         
