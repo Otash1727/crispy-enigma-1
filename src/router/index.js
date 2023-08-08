22b7917de8a445d6a83e7ded7a668e38e4ec8dfa
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-08-09 00:25:23 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../views/Dashboard.vue'
+import DefaultLayout from '../layouts/DefaultLayout.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: DefaultLayout,
+      children: [
+        {
+          path: 'Dashboard',
+          name: 'home',
+          component: Dashboard
+        },
+        {
+          path: 'about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('../views/AboutView.vue')
+        }
+      ]
+    },
+
+    // {
+    //   path: '/auth',
+    //   component: AuthLayout,
+    //   children: [
+    //     {
+    //       path: 'login',
+    //       component: () => import('../views/LoginView.vue')
+    //     },
+    //     {
+    //       path: 'register',
+    //       component: () => import('../views/RegisterView.vue')
+    //     },
+    //   ],
+    // }
+
+  ]
+})
+
+export default router
