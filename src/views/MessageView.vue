bd5ca1ef22e1bba192f927fc0f892eeda930b198
Otash1727 <128920121+Otash1727@users.noreply.github.com>
2023-08-16 16:13:33 +0300

    new fix 6

--- src/views/MessageView.vue
+++ src/views/MessageView.vue
@@ -3,7 +3,7 @@ import Message from '../components/Message.vue';
 </script>
 
 <template>
-    <header class=" bg-[#FAFAFA] flex-1 flex  flex-col font-PlusJakartaSans text-xl items-stretch overflow-y-scroll hover:overflow-scroll max-w-full scroll-smooth">
+    <header class=" bg-white flex-1 flex  flex-col font-PlusJakartaSans text-xl items-stretch overflow-y-scroll hover:overflow-scroll max-w-full scroll-smooth">
         <Message></Message>
     </header>
 </template>   
